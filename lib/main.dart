e6ec04b40053c2ddadadea6f085ee2e9b3a27954
Binup karki <karki011@gmail.com>
2020-04-19 23:02:35 -0500

    done with the basic tut app

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,96 @@ class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple[900],
       appBar: AppBar(
         title: Text('Ninja Card'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple[600],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage('https://i.pinimg.com/originals/cc/59/0d/cc590d6cce856363118c1da7f974db72.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+                height: 60,
+                color:Colors.grey[200]
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                  color: Colors.white,
+                letterSpacing: 2
+              ),
+            ),
+            SizedBox(height:10.0),
+            Text(
+              'SUBAHS KARKI',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height:10.0),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2
+              ),
+            ),
+            SizedBox(height:10.0),
+            Text(
+              '9',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+
+              ),
+            ),
+            SizedBox(height:10.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10,),
+                Text(
+                  'skarki@uab.edu',
+                  style: TextStyle(
+                    color: Colors.amberAccent[700],
+                    letterSpacing: 1.0,
+                    fontSize: 18
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const<BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+            title: Text('Home')
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Business'),
+          ),
+        ],
       ),
     );
   }

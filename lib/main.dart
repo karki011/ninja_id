f7f1287c0241ded0e7aa7da88fcc340308f4a215
Binup karki <karki011@gmail.com>
2020-04-19 22:12:04 -0500

    Inital commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: NinjaCard()
+));
+
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
